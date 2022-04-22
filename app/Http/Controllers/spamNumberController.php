773234bfb9b47ae0a01687630f55816b9e9ef526
Danko-chan <55607738+Danko-chan@users.noreply.github.com>
2022-04-22 14:31:53 +0530

    numbercontroller

---
+++ app/Http/Controllers/spamNumberController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Number;
+
+class spamNumberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $numbers = Number::orderBy('id', 'desc')->paginate(5);
+      
+        return view('numbers.index')->with('numbers', $numbers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('numbers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'number' => 'required|min:10',
+            'reported_by' => 'required'
+          ]);
+
+        $number = new Number();
+        $number->number = $request->number;
+        $number->reported_by = $request->reported_by;
+        
+
+        if ($number->save()) {
+            return redirect()->route('numbers.show', $number->id);
+          } else {
+            return redirect()->route('numbers.create');
+          }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $number = Number::findOrFail($id);
+      
+      return view('numbers.show')->with('number', $number);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/numbers/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spam Numbers</title>
+</head>
+<body>
+            <div class="container">
+                <h1>Spam Numbers</h1>
+                <hr />
+                
+                @foreach ($numbers as $number)
+                    <div class="well">
+                    <h3>{{ $number->number }}</h3>
+                    <h4>
+                        {{ $number->reported_by }}
+                    </h4>
+                @endforeach
+            </div>
+</body>
+
+</html>
