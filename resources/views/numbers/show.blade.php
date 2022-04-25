494bd25335898455cd29140b5a92f8474c4b7642
Danko-chan <55607738+Danko-chan@users.noreply.github.com>
2022-04-25 14:55:22 +0530

    1

--- resources/views/numbers/show.blade.php
+++ resources/views/numbers/show.blade.php
@@ -10,7 +10,7 @@
                 <p>
                   Submitted By: {{ $number->user->name }}, {{ $number->created_at->diffForHumans() }}
                 </p>
-                <a  class="back-btn" href="{{route('profile',Auth::user()->id)}}">&#x2190;Go Back</a>
+                <a  class="back-btn" href="{{route('numbers.create')}}">&#x2190;Go Back</a>
                 <hr style="margin-top:0; width:7%" >
 </div>
 
